a1ae020b1fc88eac6f316babfd95a983b3be6880
Usaid <usaidaqeel031@gmail.com>
2023-01-03 23:20:49 +0500

    bakend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require("./Routes/index.routes");
+
+dotenv.config({path : "./.env"})
+const PORT = process.env.PORT || 8000;
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use("/api",router)
+
+
+
+app.listen(PORT,() => console.log(`Server Runing at locahost:${PORT}`))
