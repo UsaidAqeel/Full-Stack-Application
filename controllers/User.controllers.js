91853f3237a0907f445e19b5097478dc5162f8b0
Usaid <usaidaqeel031@gmail.com>
2023-01-06 20:34:58 +0500

    Add SignUp User Func

--- controllers/User.controllers.js
+++ controllers/User.controllers.js
@@ -1,17 +1,32 @@
+const mongoose = require("mongoose");
+const userModel = require("../models/user.model");
+
 const User = {
-    createUser : (req,res) => {
-        try{
-          
+  createUser: (req, res) => {
+    try {
+      const { userName, userEmail, userPassword } = req.body;
+      if (!userName || !userEmail || !userPassword) {
+        res.statuscode;
+        res.json({ message: "some filled is missing" });
+      }
+      userModel.create(req.body, (err, data) => {
+        if (err) {
+          res.status(500);
+          res.json({ message: "Some thing is wrong" });
+        } else {
+          res.status(200);
+          res.json({ message: "user create", data });
         }
-        catch(err){
-        }
-    },
-    logInUser  : (req,res) => {
-        try{
-
-        }catch(err){
-        }
+      });
+    } catch (err) {
+      res.status(400);
+      res.json({ message: "err" });
     }
-}
+  },
+  logInUser: (req, res) => {
+    try {
+    } catch (err) {}
+  },
+};
 
-module.exports = User
+module.exports = User;
--- models/user.model.js
+++ models/user.model.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   userName: { type: String, required: true },
   userEmail: { type: String, required: true, unique: true },
   userPassword: { type: String, required: true },
+  Role: { type: String, default: "user" },
   createdAt: { type: Date, default: Date.now },
 });
 
